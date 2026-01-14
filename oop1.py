32129b52d45e4ab7e0af30092d3e3499876648b6
Rayhan Shajib <rayhanshajib500@gmail.com>
2026-01-14 20:55:49 +0200

    task 1 oop1.py

---
+++ oop1.py
@@ -0,0 +1,25 @@
+'''
+Store a personal information
+Define each person with data:
+
+ first name
+ last name
+ age
+ occupation
+
+Create a new person called Matti and assign personal details
+
+'''
+
+def personal_details():
+    firstName = "Matti"
+    lastName = "Rayhan"
+    age = 22
+    occupation = "student"
+
+    print("First Name:", firstName)
+    print("Last Name:", lastName)
+    print("Age:", age)
+    print("Occupation:", occupation)
+
+personal_details()
